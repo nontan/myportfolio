2438953fe42c46c6e3b33bb6539823469ab4c6a8
nontan <hello.rootnode@gmail.com>
2023-03-24 11:41:12 +0900

    distディレクトリをgithub pagesで表示できるか試す

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/myportfolio/'
+    base: '/myportfolio/dist/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
