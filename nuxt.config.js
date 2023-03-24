5c5fc8b24784483dcd449f91babc0fcf76bd58d9
nontan <hello.rootnode@gmail.com>
2023-03-24 12:22:03 +0900

    GitHub Pagesに表示させられるか試す

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/myportfolio/dist/'
+    base: '/myportfolio/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
